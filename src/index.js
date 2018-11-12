676e646e0760edbfda0740efc8694b0a807a63a6
Conor Turner <conorturner@live.com>
2018-11-12 21:38:09 +0000

    add better reporting of errors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,12 @@ export class Mount {
 	fireEvent(query, type) {
 		let e = document.createEvent("Event");
 		e.initEvent(type, true, true);
-		if (typeof query === "string") this.find(query)[0].dispatchEvent(e);
+		if (typeof query === "string") {
+			const el = this.find(query)[0];
+			if (!el) throw new Error(`Cannot find element: ${query}`);
+
+			el.dispatchEvent(e);
+		}
 		else query.dispatchEvent(e);
 	}
 }
